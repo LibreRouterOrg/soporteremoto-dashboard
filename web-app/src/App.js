d117976b4a8a1b14619634cfe363fd0111a696d1
German Ferrero <ferrero.gf@gmail.com>
2019-06-24 11:28:57 -0300

    Configure Ant Design with babel plugin and react-rewired.
    Also remove package-lock.json

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
@@ -10,14 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary">Ant Button</Button>
       </header>
     </div>
   );
