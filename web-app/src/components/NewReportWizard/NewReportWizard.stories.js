552b3c8cd04b0db2875849ff7513ffcfde9314d9
German Ferrero <ferrero.gf@gmail.com>
2019-07-30 15:03:15 -0300

    newreportwizard: basic implementation

---
+++ web-app/src/components/NewReportWizard/NewReportWizard.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import WizardReport, {
+    NodeSelectionStep,
+    ProblemSelectionStep,
+    ProblemBodyStep,
+} from './NewReportWizard';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+const username = 'gferrero';
+
+const nodes = ["ql-roxa", "ql-nicoyjesi", "ql-natiysofi", "si-silvia"];
+const defaultNode = ["ql-roxa"];
+
+const onLeaveBackward = action('onLeaveBackward');
+const onLeaveForward = action('onLeaveForward');
+
+const wrapperStyle = {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "80vh",
+    width: "100%",
+}
+
+storiesOf('NewReportWizard', module)
+    .addDecorator(story => <div style={wrapperStyle}>{story()}</div>)
+    .add('Node Selection', () => <NodeSelectionStep isFirst nodes={nodes} defaultNode={defaultNode} onLeaveForward={onLeaveForward} onLeaveBackward={onLeaveBackward}/>)
+    .add('Problem Body', () => <ProblemBodyStep isLast onLeaveForward={onLeaveForward} onLeaveBackward={onLeaveBackward}/>)
+    .add('Problem Selection', () => <ProblemSelectionStep onLeaveForward={onLeaveForward} onLeaveBackward={onLeaveBackward}/>)
+    .add('Interactive Wizard', () => <WizardReport nodes={nodes} defaultNode="ql-roxa" />)
