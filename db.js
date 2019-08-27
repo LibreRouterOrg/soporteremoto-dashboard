86d7fb5583f73df963e04ed254c73d4042fd3f56
gmarcos87 <gmarcos87@gmail.com>
2019-08-27 16:11:13 -0300

    add local replication

--- db.js
+++ db.js
@@ -4,10 +4,13 @@ const ssbKeys = require('ssb-keys')
 //create a secret-stack instance and add ssb-db, for persistence.
 var createSbot = require('secret-stack')({})
     .use(require('ssb-db'))
-    .use(require('scuttlebot/plugins/master'))
+    .use(require('ssb-master'))
+    .use(require('ssb-gossip'))
+    .use(require('ssb-replicate'))
     .use(require('ssb-backlinks'))
     .use(require('ssb-about'))
     .use(require('ssb-threads'))
+    .use(require('ssb-local'))
 
 // create the db instance.
 // Only one instance may be created at a time due to os locks on port and database files.
