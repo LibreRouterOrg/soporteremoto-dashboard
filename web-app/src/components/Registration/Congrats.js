63058a15a880e90971520de3a435ac0fdc565cf3
German Ferrero <ferrero.gf@gmail.com>
2019-09-02 17:12:23 -0300

    add HOC in Congrats handling navigation

--- web-app/src/components/Registration/Congrats.js
+++ web-app/src/components/Registration/Congrats.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { navigate } from '@reach/router';
 import { Typography, Button } from 'antd';
 import "./Congrats.css";
 
 const { Text } = Typography;
 
-function Congrats({ seedPhrase, username}) {
-    const seedPhrase_ = seedPhrase.join(" ");
+const api = {
+    account: {
+        get: async id => { return { name: 'gferrero' } }
+    }
+}
+
+export function Congrats({ seedPhrase, username }) {
     return (
         <div className="congrats">
             <h3>Felicitaciones {username}!</h3>
             <p>Te has registrado correctamente. Esta es tu frase secreta:</p>
-            <p><Text code>{seedPhrase_}</Text></p>
+            <p><Text code>{seedPhrase}</Text></p>
             <p><b>Conservala</b>, la necesitarás para acceder en el futuro.</p>
             <div className="button-wrapper">
                 <Button type="primary">Continuar</Button>
@@ -19,4 +25,38 @@ function Congrats({ seedPhrase, username}) {
     );
 }
 
-export default Congrats;
+class CongratsPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null,
+            seedPhrase: null,
+            loading: true,
+        }
+        this.handleExit = this.handleExit.bind(this);
+    }
+    componentDidMount() {
+        const account = api.account.isLogged();
+        const id = account['publicKey'];
+        this.setState({ seedPhrase: account['words'] });
+        api.account.get(id).then(
+            result => {
+                this.setState({
+                    username: result['name'],
+                })
+            }).finally(
+                this.setState({
+                    loading: false
+                })
+            );
+    }
+    handleExit() {
+        navigate(this.props.location.state.from);
+    }
+    render() {
+        const { seedPhrase, username } = this.state;
+        return <Congrats seedPhrase={seedPhrase} username={username} handleExit={this.handleExit} />
+    }
+}
+
+export default CongratsPage;
--- web-app/src/components/Registration/Registration.stories.js
+++ web-app/src/components/Registration/Registration.stories.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Registration from './Registration';
-import Congrats from './Congrats';
+import {Congrats} from './Congrats';
 
 const handleSumbit = action('handleSumbit');
 const nodes = ["ql-roxa", "ql-nicoyjesi", "ql-natiysofi", "si-silvia"];
-const seedPhrase = "witch collapse practice feed shame open despair creek road again ice least".split(" ");
+const seedPhrase = "witch collapse practice feed shame open despair creek road again ice least";
 
 storiesOf('Registration', module)
     .add('Default Node Roxa', () => <Registration handleSubmit={handleSumbit} nodes={nodes} defaultNode="ql-roxa"></Registration>)
