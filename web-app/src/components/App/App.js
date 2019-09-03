5e8065b3cad9fc590caad0ec8f429ce0e64e7c3d
German Ferrero <ferrero.gf@gmail.com>
2019-09-03 11:00:33 -0300

    private and public routes implementation

--- web-app/src/components/App/App.js
+++ web-app/src/components/App/App.js
@@ -1,15 +1,44 @@
 import React from 'react';
-import { Router } from '@reach/router';
+import { Router, Redirect } from '@reach/router';
 import { Row, Col } from 'antd';
+import Login from '../Login';
+import Registration from '../Registration';
+import Congrats from '../Registration/Congrats';
 import LastIssuesPanel from '../LastIssuesPanel';
 import NewReportWizard from '../NewReportWizard';
 import CommunityMap from '../CommunityMap';
 import IssuePage from '../IssuePage';
 import './App.less';
 
-function RouterComponent({ children }) {
-  return <>{children}</>;
+const api = {
+  account: {
+    isLogged: () => true
+  }
 }
+
+function PrivateRoute({ children }) {
+  /* Renders children component only if user is logged,
+    otherwise redirects to registration*/
+  if (api.account.isLogged()) {
+    return <>{children}</>;
+  } else {
+    const pathname = children.props.location.pathname;
+    return (
+      <Redirect to="/registration" state={{ from: pathname }} noThrow />
+    );
+  }
+}
+
+function NotLoggedRoute({ children }) {
+  if (api.account.isLogged()) {
+    return (
+      <Redirect to="/" noThrow />
+    );
+  } else {
+    return <>{children}</>;
+  }
+}
+
 
 function App() {
   return (
@@ -19,13 +48,20 @@ function App() {
       </Row>
       <Row id="Content">
         <Col span={16} id="MapContainer">
-          <CommunityMap/>
+          <CommunityMap />
         </Col>
         <Col span={8} id="MapInfoContainer">
-          <Router primary={false} component={RouterComponent}>
+          {/* primary and component props are set to avoid breaking style.
+              Follow https://github.com/reach/router/issues/63 for more info */}
+          <Router primary={false} component={PrivateRoute}>
             <LastIssuesPanel path="/" />
-            <NewReportWizard path="/reports/new"/>
-            <IssuePage path="/reports/:id"/>
+            <NewReportWizard path="/reports/new" />
+            <IssuePage path="/reports/:id" />
+            <Congrats path="/congrats" />
+          </Router>
+          <Router primary={false} component={NotLoggedRoute}>
+            <Registration path="/registration" />
+            <Login path="/login" />
           </Router>
         </Col>
       </Row>
