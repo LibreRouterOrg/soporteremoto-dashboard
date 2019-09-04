c015254afd4c90bb07f0a5c2d85751bacbba1567
gmarcos87 <gmarcos87@gmail.com>
2019-09-04 13:31:40 -0300

    add db-config file

---
+++ db-config.js
@@ -0,0 +1,27 @@
+const Path = require('path')
+const Config = require('ssb-config/inject')
+const ssbKeys = require('ssb-keys')
+
+const config = Config('soporteremoto', {
+    caps: {
+        shs: 'v/vqlQfe1EGN5gi187Wpl+0RIaeGaNkjGeoHtjmcouA=',
+        sign: null,
+        invite: 'VSPZS69Yqzz2F6E+TaQBKjxJqJGXAfkRKwnCbSjvy70='
+    },
+    app_name: 'soporteremoto',
+    server: true,
+    local: true
+});
+
+config.keys = ssbKeys.loadOrCreateSync(Path.join(config.path, 'secret'))
+const pubkey = config.keys.id.slice(1).replace(`.${config.keys.curve}`, '')
+
+const socketPath = Path.join(config.path, 'socket')
+config.connections.incoming.unix = [{
+    scope: 'device',
+    transform: 'noauth'
+}]
+
+config.remote = `unix:${socketPath}:~noauth:${pubkey}`
+
+module.exports = config;
