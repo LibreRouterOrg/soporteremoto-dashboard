6988a443127ec2e1f5b6f82ce7d38a03b16f9557
gmarcos87 <gmarcos87@gmail.com>
2019-09-04 13:54:50 -0300

    change ssb config and plugins

--- db.js
+++ db.js
@@ -1,29 +1,27 @@
-const pull = require('pull-stream')
-const ssbKeys = require('ssb-keys')
+
+const SecretStack = require('secret-stack')
+const config = require('./db-config')
 
 //create a secret-stack instance and add ssb-db, for persistence.
-var createSbot = require('secret-stack')({})
-    .use(require('ssb-db'))
-    .use(require('ssb-master'))
-    .use(require('ssb-gossip'))
-    .use(require('ssb-replicate'))
-    .use(require('ssb-backlinks'))
-    .use(require('ssb-about'))
-    .use(require('ssb-threads'))
-    .use(require('ssb-local'))
+var CreateSBot = SecretStack()
+ .use(require('ssb-db'))
+ .use(require('ssb-lan'))
+ .use(require('ssb-conn'))
+ .use(require('ssb-master'))
+ .use(require('ssb-replicate'))
+ .use(require('ssb-friends'))
+.use(require('ssb-backlinks'))
+.use(require('ssb-about'))
+.use(require('ssb-threads'))
+.use(require('ssb-promiscuous'))
 
 // create the db instance.
 // Only one instance may be created at a time due to os locks on port and database files.
-var sbot = createSbot(require('ssb-config/inject')('soporteremoto',{
-    port: 8091,
-    caps: {
-      shs: 'v/vqlQfe1EGN5gi187Wpl+0RIaeGaNkjGeoHtjmcouA=',
-      invite: 'VSPZS69Yqzz2F6E+TaQBKjxJqJGXAfkRKwnCbSjvy70='
-    }
-}))
+var sbot = CreateSBot(config)
 
-//setTimeout(()=>{
-//
-//},1000)
+// Start peer connections
+setTimeout(()=>{
+  sbot.conn.start()  
+}, 1000)
 
 export default sbot
