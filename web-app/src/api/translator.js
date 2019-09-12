69c274fb635eee943e4eb8a8feef2313974b255b
gmarcos87 <gmarcos87@gmail.com>
2019-09-12 20:35:33 -0300

    add transalator
    this functions add more info or change the api response

---
+++ web-app/src/api/translator.js
@@ -0,0 +1,25 @@
+import api from ".";
+
+export const formatReport  = async({messages, full}) => {
+    const { timestamp, author, content } = messages[0].value;
+    const { node, common_issue, title, body}  = content
+    const user = await api.account.get(author);
+    return {
+        key: messages[0].key,
+        timestamp,
+        user,
+        status: 'open',
+        node,
+        commonIssueId: common_issue.id,
+        title,
+        body,
+        comments: messages.splice(0,1).map(msg => msg.key)
+    }
+}
+
+export const formatUser = (account) => ({
+  username: account.name,
+  avatar: account.avatar,
+  node: account.node,
+  key: account.key,
+})
