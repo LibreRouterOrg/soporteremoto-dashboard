f615df3f775a763ad8f1935fe58474cf7a3db125
gmarcos87 <gmarcos87@gmail.com>
2019-09-19 15:41:09 -0300

    fix missing export

--- federated-server/routes/account/index.js
+++ federated-server/routes/account/index.js
@@ -1,4 +1,5 @@
 export const createAccount = require('./create').createAccount
 export const getAccount = require('./get').getAccount
+export const setAccount = require('./set').setAccount
 export const getSequence = require('./getSequence').getSequence
 export const listAccounts = require('./list').listAccounts
