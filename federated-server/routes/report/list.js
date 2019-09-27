7da95a789e6df837945abc08f6c653fac95f47ef
gmarcos87 <gmarcos87@gmail.com>
2019-09-27 15:07:26 -0300

    add more readability and documentation

--- federated-server/routes/report/list.js
+++ federated-server/routes/report/list.js
@@ -15,15 +15,12 @@ export const listReports = (req,res) => {
         req.body.gt = d.getTime();
         req.body.lt = Date.now();
     }
-    
+
     pull(
         sbot.threads.public({allowlist: 'reports'}),
-        pull.filter(onlyValidThreads),
+        pull.filter(onlyValidThreas),
         pull.map(removeInvalidMsg),
-        pull.asyncMap(async(data, cb) => { 
-            data.messages[0].value.content.status = await getStatus(data.messages[0].key)
-            cb(null, data)
-        }),
+        pull.asyncMap(injectStatus),
         pull.collect((err, msgs) => {
             err
                 ? res.json({error: 'object not found'})
@@ -32,10 +29,18 @@ export const listReports = (req,res) => {
     )
 }
 
+/* Add the last status to the report. */
+async function injectStatus(data, cb) {
+    data.messages[0].value.content.status = await getStatus(data.messages[0].key)
+    cb(null, data)
+}
+
+/* Check that the first message of the thread is correctly formulated and is valid. */
 function onlyValidThreads({messages = []}) {
     return messages.length > 0 && messages[0].value && messages[0].value.content && messages[0].value.content.common_issue
 }
 
+/* Removes from the thread messages that are not comments or report messages */ 
 function removeInvalidMsg({messages = [], full}) {
     return { full, messages: messages.filter(msg => msg.value)}
 }
