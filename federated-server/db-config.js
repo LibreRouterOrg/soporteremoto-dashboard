ac98a11c6c0c392f57c27135bf2fce125dd6b0f9
Marcos Gutierrez <gmarcos87@gmail.com>
2019-10-10 11:34:59 -0300

    add load or create server config

--- federated-server/db-config.js
+++ federated-server/db-config.js
@@ -1,33 +1,34 @@
 const Path = require('path')
 const Config = require('ssb-config/inject')
 const ssbKeys = require('ssb-keys')
+const { loadOrCreateConfig } = require('./config')
 
-const { APP_NAME, SHS, INVITE} = process.env
-
-const config = Config(APP_NAME || 'soporteremoto', {
-    caps: {
-        shs: SHS || 'v/vqlQfe1EGN5gi187Wpl+0RIaeGaNkjGeoHtjmcouA=',
-        sign: null,
-        invite: INVITE || 'VSPZS69Yqzz2F6E+TaQBKjxJqJGXAfkRKwnCbSjvy70='
-    },
-    app_name: APP_NAME || 'soporteremoto',
-    server: true,
-    local: true
-});
-
-config.keys = ssbKeys.loadOrCreateSync(Path.join(config.path, 'secret'))
-const pubkey = config.keys.id.slice(1).replace(`.${config.keys.curve}`, '')
-
-const socketPath = Path.join(config.path, 'socket')
-config.connections.incoming.unix = [{
-    scope: 'device',
-    transform: 'noauth'
-}]
-
-//ssb-rooms
-config.connections.incoming.tunnel = [{scope: 'public', transform: 'shs'}]
-config.connections.outgoing.tunnel = [{transform: 'shs'}]
-
-config.remote = `unix:${socketPath}:~noauth:${pubkey}`
-
-module.exports = config;
+export async function getSsbConfig(){
+    const {network} = await loadOrCreateConfig()
+    const config = Config('soporteremoto', {
+        caps: {
+            shs: network.secretHash,
+            sign: null,
+            invite: network.secretInvite
+        },
+        app_name: 'soporteremoto',
+        server: true,
+        local: true
+    });
+    
+    config.keys = ssbKeys.loadOrCreateSync(Path.join(config.path, 'secret'))
+    const pubkey = config.keys.id.slice(1).replace(`.${config.keys.curve}`, '')
+    
+    const socketPath = Path.join(config.path, 'socket')
+    config.connections.incoming.unix = [{
+        scope: 'device',
+        transform: 'noauth'
+    }]
+    
+    //ssb-rooms
+    config.connections.incoming.tunnel = [{scope: 'public', transform: 'shs'}]
+    config.connections.outgoing.tunnel = [{transform: 'shs'}]
+    
+    config.remote = `unix:${socketPath}:~noauth:${pubkey}`
+    return config;    
+}
--- federated-server/routes/network/getNodes.js
+++ federated-server/routes/network/getNodes.js
@@ -1,9 +1,11 @@
-import { genericId } from "../../config";
-import sbot from '../../db'
+import { loadOrCreateConfig } from "../../config";
+import sbot from '../../db';
 import pull from 'pull-stream';
 import { flatToUnique } from "../../utils/faltToUnique";
 
-export const getNodes = (req, res) => {
+export const getNodes = async function(req, res) {
+    const { network } = await loadOrCreateConfig();
+    const genericId = network.communityKeys.public;
     pull(
         sbot.about.read({ dest: genericId, old: true, live: false }),
         pull.filter(({value}) => value.content.nodes),
