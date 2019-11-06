5c82aeb5305d15c84af13dc484191c7f32deeebe
Marcos Gutierrez <gmarcos87@gmail.com>
2019-11-06 09:15:44 -0300

    fix sendNodeToDb function

--- federated-server/routes/network/getNodes.js
+++ federated-server/routes/network/getNodes.js
@@ -1,16 +1,18 @@
 import { loadOrCreateConfig } from "../../config";
-import sbot from '../../db';
+import { getSbotAsPromise } from "../../db";
 import pull from 'pull-stream';
 import { flatToUnique } from "../../utils/faltToUnique";
 
 export const getNodes = async function(req, res) {
+    const sbot = await getSbotAsPromise()
     const { network } = await loadOrCreateConfig();
-    const genericId = network.communityKeys.public;
+    const genericId = network.communityKeys.id;
     pull(
         sbot.about.read({ dest: genericId, old: true, live: false }),
         pull.filter(({value}) => value.content.nodes),
         pull.map(({value}) => value.content.nodes),
         pull.collect((error, results = []) => {
+            console.log(error, results)
             if (error) { return; }
             const nodes = results.reduce(flatToUnique,[])
             res.json({nodes});
