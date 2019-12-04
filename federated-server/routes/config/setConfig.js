4a9d7c3b2cbc6c6f18b40b3fa793a6abffb10ec8
Marcos Gutierrez <gmarcos87@gmail.com>
2019-12-04 16:38:42 -0300

    save pubCert in config

--- federated-server/routes/config/setConfig.js
+++ federated-server/routes/config/setConfig.js
@@ -1,18 +1,24 @@
 import { makePasswd } from "../../utils/makePassword"
 import { getSbot } from "../../db"
-import { loadOrCreateConfig } from "../../config"
+import { loadOrCreateConfig, saveConfig } from "../../config"
 import { writeSecretBox, formPrivate, writePublic } from "../../utils/signify"
 
 export const setConfig = async (req, res) => {
     const password = makePasswd()
     const communityName = req.body.name
 
-    const { network } = await loadOrCreateConfig();
-    const genericId = network.communityKeys.id;
+    const config = await loadOrCreateConfig();
+    const genericId = config.network.communityKeys.id;
     const { secret, unsecret } = writeSecretBox(genericId, password)
 
     const pubCert = writePublic(formPrivate(unsecret).publicKey, true)
 
+    saveConfig({
+      ...config,
+      certificates: {
+        pubCert
+      }
+    });
 
     getSbot(async (sbot)=> {
       const certificates = {
