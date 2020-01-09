9502e1e51a2e52ac5fdea353202126cebddd43ae
German Ferrero <ferrero.gf@gmail.com>
2020-01-09 11:29:09 -0300

    remove wrong import

--- federated-server/routes/supportRequests/index.js
+++ federated-server/routes/supportRequests/index.js
@@ -1,4 +1,3 @@
 export const createSupportRequest = require('./create').createSupportRequest
-export const getSupportRequest = require('./get').getSupportRequest
 export const setSupportRequest = require('./set').setSupportRequest
 export const listSupportRequests = require('./list').listSupportRequests
